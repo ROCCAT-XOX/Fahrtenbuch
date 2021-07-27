43e68802b6bdea7ae6521564085b08400b04c612
Felix Harz <felixharz99@gmail.com>
2021-07-27 11:50:35 +0200

    added login functionality

--- app/src/main/java/com/example/fahrtenbuch/Login.java
+++ app/src/main/java/com/example/fahrtenbuch/Login.java
@@ -4,24 +4,107 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Authenticator;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Credentials;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.Route;
 
 public class Login extends AppCompatActivity {
     private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_login);
+
+        EditText email = findViewById(R.id.et_email);
+        EditText password = findViewById(R.id.et_password);
 
         loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               switchActivity();
+                try {
+                    fetch("http://10.0.2.2:5000/login", email.getText().toString(), password.getText().toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
+
+    private OkHttpClient createAuthenticatedClient(final String username,
+                                                   final String password) {
+        // build client with authentication information.
+        OkHttpClient httpClient = new OkHttpClient.Builder().authenticator(new Authenticator() {
+            public Request authenticate(Route route, Response response) throws IOException {
+                String credential = Credentials.basic(username, password);
+                return response.request().newBuilder().header("Authorization", credential).build();
+            }
+        }).build();
+        Log.d("My_Application", "header");
+        return httpClient;
+    }
+
+    private void doRequest(OkHttpClient httpClient, String anyURL) throws Exception {
+
+        Request request = new Request.Builder()
+                .url(anyURL)
+                .build();
+
+        httpClient.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    //final String myResponse = response.body().string();
+
+                    Login.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(Login.this, "This is my Toast message!", Toast.LENGTH_SHORT).show();
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                public void run() {
+                                    switchActivity();
+                                }
+                            }, 1500);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    public void fetch(String url, String username, String password) throws Exception {
+
+        OkHttpClient httpClient = createAuthenticatedClient(username, password);
+        // execute request
+        doRequest(httpClient, url);
+    }
+
+
     private void switchActivity(){
         Intent switchActivityIntent = new Intent(this, Welcome.class);
         startActivity(switchActivityIntent);
