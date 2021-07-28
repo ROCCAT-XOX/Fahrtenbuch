3585246084bd00b36980fabd53bb0df852a1d6bc
RoccatXOX <yannickstumpf17@gmail.com>
2021-07-28 19:29:14 +0200

    Style changes and add some new Infos to Fleet

---
+++ app/src/main/java/com/example/fahrtenbuch/correction.java
@@ -0,0 +1,14 @@
+package com.example.fahrtenbuch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class correction extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_correction);
+    }
+}
